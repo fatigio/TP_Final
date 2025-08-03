9942ac2702c0604ba542ac79b64e0942a0adb296
fatigio <fatigiorgi05@gmail.com>
2025-08-03 19:21:24 -0300

    tp-final

--- src/Screens/CommunityScreen/CommunityScreen.jsx
+++ src/Screens/CommunityScreen/CommunityScreen.jsx
@@ -7,7 +7,7 @@ const CommunityScreen = () => {
         <div className="screens-container">
             <ICONS.Community className="icon-screens"/>
             <h1 className="screens-title">Crea comunidades</h1>
-            <span className="screens-description">Crea grupos para reunir a los miembros en función de temas y envíales fácilmente avisos al avisos del administrador.</span>
+            <span className="screens-description">Crea grupos para reunir a los miembros en función de temas y envíales fácilmente avisos del administrador.</span>
         </div>
     )
 }
